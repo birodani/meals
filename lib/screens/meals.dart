226e8aa57d6ef3b347a49d21881929f6f294014f
birodani <daniel.biro@novaservices.hu>
2024-06-21 08:02:48 +0200

    feat: improve category grid, add meal item

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:meals/main.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.meals});
@@ -11,7 +11,7 @@ class MealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Widget content =
-        ListView.builder(itemCount: meals.length, itemBuilder: (ctx, index) => Text(meals[index].title));
+        ListView.builder(itemCount: meals.length, itemBuilder: (ctx, index) => MealItem(meal: meals[index]));
     if (meals.isEmpty) {
       content = Center(
         child: Column(
--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -4,14 +4,17 @@ import 'package:meals/models/category.dart';
 class CategoryGridItem extends StatelessWidget {
   const CategoryGridItem({
     super.key,
-    required this.category,
+    required this.category, 
+    required this.onSelectCategory,
   });
+
   final Category category;
+  final Function() onSelectCategory;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {},
+      onTap: () {onSelectCategory();},
       splashColor: Theme.of(context).primaryColor,
       borderRadius: BorderRadius.circular(16),
       child: Container(
