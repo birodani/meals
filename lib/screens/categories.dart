f044320a334724abc43556a653265c78f898dee1
birodani <daniel.biro@novaservices.hu>
2024-06-23 15:07:56 +0200

    reafactor: use SlideTransition

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -23,8 +23,7 @@ class _CategoriesScreenState extends State<CategoriesScreen>
         vsync: this,
         duration: const Duration(milliseconds: 300),
         lowerBound: 0,
-        upperBound: 1
-    );
+        upperBound: 1);
 
     _animationController.forward();
   }
@@ -53,29 +52,33 @@ class _CategoriesScreenState extends State<CategoriesScreen>
   Widget build(BuildContext context) {
     return Scaffold(
       body: AnimatedBuilder(
-        animation: _animationController,
-        child: GridView(
-          padding: const EdgeInsets.all(24),
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 20,
-            mainAxisSpacing: 20,
+          animation: _animationController,
+          child: GridView(
+            padding: const EdgeInsets.all(24),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 3 / 2,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20,
+            ),
+            children: [
+              ...availableCategories.map(
+                (category) => CategoryGridItem(
+                  category: category,
+                  onSelectCategory: () {
+                    _selectCategory(context, category);
+                  },
+                ),
+              )
+            ],
           ),
-          children: [
-            ...availableCategories.map(
-              (category) => CategoryGridItem(
-                category: category,
-                onSelectCategory: () {
-                  _selectCategory(context, category);
-                },
-              ),
-            )
-          ],
-        ),
-        builder: (context, child) =>
-          Padding(padding: EdgeInsets.only(top: 100 - _animationController.value * 100), child: child,)
-      ),
+          builder: (context, child) => SlideTransition(
+                position:
+                    Tween(begin: const Offset(0, 0.4), end: const Offset(0, 0))
+                        .animate(CurvedAnimation(
+                            parent: _animationController, curve: Curves.easeInOut)),
+                child: child,
+              )),
     );
   }
 }
